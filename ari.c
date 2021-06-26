d25531bfc835552dec8ed39187e8742c197d867c
mathiyalagankalyanasundaram <84658698+mathiyalagankalyanasundaram@users.noreply.github.com>
2021-06-26 12:38:00 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cs50.h>
-#include <ctype.h>
+#include<ctype.h>
 
 
+string ari(string n){
 
-int main(){
-int i,formula;
-float letter=0.0,word=0.0,sentance=0.0;
- string n="The town was large enough to support three inns, and all their common rooms were rife with rumors. Arianne sent her men into each of them, to hear what they might hear. In the Broken Shield, Daemon Sand was told that the great septry on the Holf of Men had been burned and looted by raiders from the sea, and a hundred young novices from the motherhouse on Maiden Isle carried off into slavery.";
+  int i,formula;
+  float letter=0.0,word=0.0,sentance=0.0;
   for(i=0;i<strlen(n);i++){
       if(isalnum(n[i])){
       letter=letter+1;}
@@ -17,42 +16,41 @@ float letter=0.0,word=0.0,sentance=0.0;
       word=word+1;}
        if(n[i]=='?'||n[i]=='.'||n[i]=='!'){
          sentance=sentance+1;}
-      }
+  }
    
 
     formula=4.71*(letter/word)+0.5*(word/sentance)-21.43+1;
+    
 
     switch (formula){
-        case 1: printf("kindergarden");
+        case 1: printf("Kindergarten");
             break;
-        case 2: printf("first or second class");
+        case 2: printf("First/Second Grade");
             break;
-        case 3: printf("third grade");
+        case 3: printf("Third Grade");
             break;
-        case 4: printf("fourth grade");
+        case 4: printf("Fourth Grade");
             break;
-        case 5: printf("fiveth grade");
+        case 5: printf("Fifth Grade");
             break;
-        case 6: printf("sixth grade");
+        case 6: printf("Sixth Grade");
             break;
-        case 7: printf("seventh grade");
+        case 7: printf("Seventh Grade");
             break;
-        case 8: printf("eighth grade");
+        case 8: printf("Eighth Grade");
             break;
-        case 9: printf("ningth grade");
+        case 9: printf("Ninth Grade");
             break;
-        case 10: printf("tenth grade");
+        case 10: printf("Tenth Grade");
             break;
-        case 11: printf("eleventh grade");
+        case 11: printf("Eleventh Grade");
             break;
-        case 12: printf("twelth grade");
+        case 12: printf("Twelfth grade");
             break;
-        case 13: printf("college student");
+        case 13: printf("College student");
             break;
-        case 14: printf("professor"); 
+        case 14: printf("Professor"); 
             break;
     }
-
-
 
 }
